2fe56778a3791a083cdbb2f23c01ffd4cc6031ff
haint <hainguyenth2003@gmail.com>
2025-08-13 14:54:56 +0700

    feat(flash): turn on/off flash of device

--- app/src/main/java/com/example/flashlight/fragment/FlashFragment.kt
+++ app/src/main/java/com/example/flashlight/fragment/FlashFragment.kt
@@ -1,16 +1,18 @@
 package com.example.flashlight.fragment
 
+import android.content.Context
+import android.hardware.camera2.CameraManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.flashlight.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -19,15 +21,10 @@ private const val ARG_PARAM2 = "param2"
  */
 class FlashFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var isFlashOn = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -38,23 +35,37 @@ class FlashFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_flash, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FlashFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FlashFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val flashButton = view.findViewById<ImageView>(R.id.flashButton)
+
+        flashButton.setOnClickListener {
+            toggleFlashlight(requireContext(), flashButton)
+        }
+    }
+
+    private fun toggleFlashlight(context: Context, flashButton: ImageView) {
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+
+        isFlashOn = !isFlashOn
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                cameraManager.setTorchMode(cameraId, isFlashOn)
             }
+//            flashButton.setImageResource(
+//                if (isFlashOn) R.drawable.outline_flashlight_on_96
+//                else R.drawable.outline_flashlight_on_96
+//            )
+            if (isFlashOn) {
+                flashButton.setImageResource(R.drawable.outline_flashlight_off_96) // icon tắt đèn
+            } else {
+                flashButton.setImageResource(R.drawable.outline_flashlight_on_96) // icon bật đèn
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
