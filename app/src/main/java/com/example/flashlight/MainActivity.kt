8296bea69490e1da20a848fcd715c44239d7453f
haint <hainguyenth2003@gmail.com>
2025-08-13 14:31:11 +0700

    feat(UI): Add bottom app bar & flash fragment

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -9,6 +9,9 @@ import com.example.flashlight.fragment.FlashFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +19,44 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        bottomNavigation()
+    }
+
+    fun bottomNavigation() {
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+
+        // Load fragment mặc định
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, FlashFragment())
+            .commit()
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            val selectedFragment = when (item.itemId) {
+                R.id.bottom_home -> FlashFragment()
+                R.id.bottom_search -> FlashFragment()
+                R.id.bottom_settings -> FlashFragment()
+                R.id.bottom_profile -> FlashFragment()
+                else -> null
+            }
+            selectedFragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frame_layout, it)
+                    .commit()
+            }
+            true
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNavigation) { view, insets ->
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(
+                view.paddingLeft,
+                view.paddingTop,
+                view.paddingRight,
+                0
+            )
+            // Trả lại insets không bị tiêu thụ
             insets
         }
     }
