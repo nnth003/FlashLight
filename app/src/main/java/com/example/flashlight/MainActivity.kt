1819ddc1320dc9b93f65dc2c3cd8953aa96676c1
haint <hainguyenth2003@gmail.com>
2025-08-14 09:45:29 +0700

    Add navigation and fix ui

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.flashlight.fragment.FlashFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,27 +31,32 @@ class MainActivity : AppCompatActivity() {
 
     fun bottomNavigation() {
         bottomNavigation = findViewById(R.id.bottomNavigation)
+        val navHostController =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostController.navController
 
-        // Load fragment mặc định
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frame_layout, FlashFragment())
-            .commit()
+        bottomNavigation.setupWithNavController(navController)
 
-        bottomNavigation.setOnItemSelectedListener { item ->
-            val selectedFragment = when (item.itemId) {
-                R.id.bottom_home -> FlashFragment()
-                R.id.bottom_search -> FlashFragment()
-                R.id.bottom_settings -> FlashFragment()
-                R.id.bottom_profile -> FlashFragment()
-                else -> null
-            }
-            selectedFragment?.let {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frame_layout, it)
-                    .commit()
-            }
-            true
-        }
+//        // Load fragment mặc định
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.frame_layout, FlashFragment())
+//            .commit()
+//
+//        bottomNavigation.setOnItemSelectedListener { item ->
+//            val selectedFragment = when (item.itemId) {
+//                R.id.bottom_home -> FlashFragment()
+//                R.id.bottom_search -> FlashFragment()
+//                R.id.bottom_settings -> FlashFragment()
+//                R.id.bottom_profile -> FlashFragment()
+//                else -> null
+//            }
+//            selectedFragment?.let {
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.frame_layout, it)
+//                    .commit()
+//            }
+//            true
+//        }
 
         ViewCompat.setOnApplyWindowInsetsListener(bottomNavigation) { view, insets ->
             val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
